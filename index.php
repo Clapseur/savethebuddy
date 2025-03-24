b0b0ce83995fc4d84838aacf0b9d4fbc1b69efb4
echimine <eliott.chiminello@gmail.com>
2025-03-24 16:19:06 +0100

    first commit added prototype

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+
+    <!-- gsap -->
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
+    <title>Worshop3</title>
+</head>
+
+<body>
+    <form action="POST">
+        <div class="table-iframe">
+            <iframe class="module1" src="module1/index.html" frameborder="2"></iframe>
+            <iframe class="module2" src="module1/index.html" frameborder="2"></iframe>
+            <iframe class="module3" src="module1/index.html" frameborder="2"></iframe>
+            <iframe class="module4" src="module1/index.html" frameborder="2"></iframe>
+        </div>
+        <button>joué</button>
+        <button>new game</button>
+    </form>
+
+</body>
+<script src="script.js"></script>
+
+</html>
